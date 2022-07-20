8573c0010317ecce0c7beb860636b064938f9bec
riku <riku@aleph.engineering>
2022-07-19 20:35:07 -0500

    Implement Store.findProducts

--- src/main/java/com/market/store/domain/Store.java
+++ src/main/java/com/market/store/domain/Store.java
@@ -1,9 +1,12 @@
 package com.market.store.domain;
 
+import com.market.core.domain.search.PageSearch;
 import com.market.core.util.Asserts;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -17,6 +20,10 @@ public class Store {
         Asserts.assertNonNullOrEmpty(product.getName(), "Product Name cant be empty.");
 
         return productRepository.saveOrUpdate(product);
+    }
+
+    public List<Product> findProducts(PageSearch search) {
+        return productRepository.findAll(search).stream().toList();
     }
 
 }
