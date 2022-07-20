dd72d355231b750887cfdef08e35e85f6d7d0393
riku <riku@aleph.engineering>
2022-07-19 21:37:17 -0400

    Implement POST /store/product/search advance search

--- src/main/java/com/market/store/api/StoreController.java
+++ src/main/java/com/market/store/api/StoreController.java
@@ -1,15 +1,15 @@
 package com.market.store.api;
 
+import com.market.core.domain.search.PageSearch;
 import com.market.store.api.dto.ProductDTO;
 import com.market.store.domain.Store;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/store")
@@ -24,5 +24,15 @@ public class StoreController {
         var storedProduct = store.addProduct(productDTO.toEntity());
 
         return ResponseEntity.ok(ProductDTO.from(storedProduct));
+    }
+
+    @PostMapping("/product/search")
+    public ResponseEntity<List<ProductDTO>> getProduct(@RequestBody(required = false) PageSearch search) {
+        if (Objects.isNull(search))
+            search = new PageSearch();
+
+        var products = store.findProducts(search).stream().map(ProductDTO::from).toList();
+
+        return ResponseEntity.ok(products);
     }
 }
