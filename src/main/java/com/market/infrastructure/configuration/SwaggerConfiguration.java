b713e3de30f39d55386e7c4fa75a02db1eb16ee1
riku <riku@aleph.engineering>
2023-09-18 13:12:01 -0400

    Set Api basic information
    Title, version, Description

--- src/main/java/com/market/infrastructure/configuration/SwaggerConfiguration.java
+++ src/main/java/com/market/infrastructure/configuration/SwaggerConfiguration.java
@@ -2,6 +2,7 @@ package com.market.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -11,7 +12,14 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class SwaggerConfiguration {
     @Bean
     public Docket api() {
+        var apiInfo = new ApiInfoBuilder()
+                .title("MarketJ")
+                .version("1.1")
+                .description("Simple market develop on Java")
+                .build();
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
