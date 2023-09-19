148b8eb5f7631cfb3eb5fdf7e5010577bc119359
riku <riku@aleph.engineering>
2023-09-19 16:41:59 -0400

    Redirect / to swagger

--- src/main/java/com/market/infrastructure/configuration/SwaggerConfiguration.java
+++ src/main/java/com/market/infrastructure/configuration/SwaggerConfiguration.java
@@ -3,6 +3,8 @@ package com.market.infrastructure.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,7 +12,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SwaggerConfiguration {
+public class SwaggerConfiguration implements WebMvcConfigurer {
 
     @Value("${application-version}")
     private String version = "v1";
@@ -29,5 +31,11 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/v1/**"))
                 .build();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/swagger-ui/");
+        registry.addRedirectViewController("/swagger-ui", "/swagger-ui/");
     }
 }
