64d39761c6e74eaac21e3fc96a62a2dfcc6ca9ea
riku <riku@aleph.engineering>
2023-09-20 15:22:47 -0400

    Make final and hide constructor from Util classes

--- src/main/java/com/market/core/util/Arrays.java
+++ src/main/java/com/market/core/util/Arrays.java
@@ -1,6 +1,10 @@
 package com.market.core.util;
 
-public class Arrays {
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Arrays {
     public static <T> T getPositionOr(T[] array, int position, T defaultValue) {
         if (position - 1 < array.length)
             return array[position];
