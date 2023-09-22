5476b30d7e79ce04a77684b0fcd8866bf4ef8d57
riku <riku@aleph.engineering>
2023-09-22 16:01:51 -0400

    Avoid set on BaseInformation.creationDate

--- src/main/java/com/market/core/domain/BaseInformation.java
+++ src/main/java/com/market/core/domain/BaseInformation.java
@@ -7,8 +7,9 @@ import java.util.Date;
 
 
 @Getter
-@Setter
 public abstract class BaseInformation {
-    protected Date creationDate = new Date();
+    protected Date creationDate;
+
+    @Setter
     protected Date lastChange;
 }
