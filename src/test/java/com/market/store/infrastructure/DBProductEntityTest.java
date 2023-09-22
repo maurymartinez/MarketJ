c98aa918ed3b0de8cb9d8bb54e99a8c53408d6e2
riku <riku@aleph.engineering>
2023-09-22 16:00:32 -0400

    Add DBProductEntity tests

---
+++ src/test/java/com/market/store/infrastructure/DBProductEntityTest.java
@@ -0,0 +1,43 @@
+package com.market.store.infrastructure;
+
+import com.market.util.ClassUtil;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class DBProductEntityTest {
+
+    @Test
+    void toDomain() {
+        var dbProduct = ClassUtil.generator.nextObject(DBProductEntity.class);
+
+        var product = dbProduct.toDomain();
+
+        assertThat(dbProduct.getCreationDate()).isEqualTo(product.getCreationDate());
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {DBProductEntity.class})
+    void setGetPairTest(Class<?> source) throws InvocationTargetException, IllegalAccessException {
+        var setValues = new HashMap<String, Object>();
+        var obj = ClassUtil.generator.nextObject(source);
+
+        for (var method : obj.getClass().getMethods())
+            if (method.getName().startsWith("set")) {
+                var value = ClassUtil.generator.nextObject(method.getParameterTypes()[0]);
+                method.invoke(obj, value);
+                setValues.put(method.getName().replace("set", "get"), value);
+                setValues.put(method.getName().replace("set", "is"), value);
+            }
+
+        for (var method : obj.getClass().getMethods())
+            if (setValues.containsKey(method.getName()))
+                assertThat(method.invoke(obj)).isEqualTo(setValues.get(method.getName()));
+    }
+}
