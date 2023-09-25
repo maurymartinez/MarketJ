b5e1cd4173a36ad85b03aa04cdcbbb457fa29024
riku <riku@aleph.engineering>
2023-09-25 16:51:31 -0400

    Add Product constructor from ProductValue

--- src/main/java/com/market/store/domain/Product.java
+++ src/main/java/com/market/store/domain/Product.java
@@ -2,33 +2,35 @@ package com.market.store.domain;
 
 import com.market.core.domain.EntityBaseInformation;
 import com.market.core.util.Asserts;
+import com.market.store.domain.value.ProductValue;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 
 @Getter
-@Setter(AccessLevel.PRIVATE)
 @NoArgsConstructor
-@AllArgsConstructor
 public class Product extends EntityBaseInformation implements Cloneable {
 
-    public Product(String id, String serial, String name, String type, Collection<String> tags, double price, boolean sold) {
-        this.id = id;
-        this.serial = serial;
-        this.name = name;
-        this.type = type;
-        this.tags = tags;
-        this.price = price;
-        this.sold = sold;
+    Product(ProductValue value) {
+        id = value.id();
+        serial = value.serial();
+        name = value.name();
+        type = value.type();
+        tags = value.tags();
+        price = value.price();
+        sold = value.sold();
+        creationDate = value.creationDate();
+        lastChange = value.lastModification();
     }
 
     private String serial;
     private String name;
 
     private String type;
-    private Collection<String> tags;
+    private Collection<String> tags = new ArrayList<>();
     private double price;
     private boolean sold;
 
@@ -36,7 +38,7 @@ public class Product extends EntityBaseInformation implements Cloneable {
         Asserts.assertIfNot(sold, String.format("Product %s has already been sold.", id));
 
         var product = this.clone();
-        product.setSold(Boolean.TRUE);
+        product.sold = Boolean.TRUE;
 
         return product;
     }
@@ -45,7 +47,7 @@ public class Product extends EntityBaseInformation implements Cloneable {
     public Product clone() {
         try {
             var clone = (Product) super.clone();
-            clone.setTags(List.copyOf(tags));
+            clone.tags = List.copyOf(tags);
             return clone;
         } catch (CloneNotSupportedException e) {
             throw new AssertionError();
